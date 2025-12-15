8a0b639e2fa61e1e979bc11b3660ca456368b2bd
Home <home@Homes-MacBook-Air.local>
2025-12-15 09:58:32 -0800

    first init

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-function wordflow_enqueue_styles() {
-  wp_enqueue_style('wordflow-style', get_stylesheet_uri());
+function wordwind_enqueue_styles() {
+  wp_enqueue_style('wordwind-style', get_stylesheet_uri());
   wp_enqueue_style('tailwind-output', get_template_directory_uri() . '/src/output.css');
 }
-add_action('wp_enqueue_scripts', 'wordflow_enqueue_styles');
+add_action('wp_enqueue_scripts', 'wordwind_enqueue_styles');
