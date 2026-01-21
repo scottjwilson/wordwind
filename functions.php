07654991662556f1e4d715ca3dbf6a59ccde22e3
Scott Wilson <scottwildev@gmail.com>
2026-01-21 10:13:04 -0800

    ballstreet types included in functions

--- functions.php
+++ functions.php
@@ -4,12 +4,13 @@
  */
 
 // Theme setup (enqueue scripts/styles, theme support, menus)
-require_once get_template_directory() . '/inc/theme-setup.php';
+require_once get_template_directory() . "/inc/theme-setup.php";
 
 // Custom nav walker for Tailwind styling
-require_once get_template_directory() . '/inc/class-tailwind-walker.php';
+require_once get_template_directory() . "/inc/class-tailwind-walker.php";
 
 // Template functions
-require_once get_template_directory() . '/inc/template-sections.php';
-require_once get_template_directory() . '/inc/template-athletes.php';
-require_once get_template_directory() . '/inc/template-functions.php';
+require_once get_template_directory() . "/inc/template-sections.php";
+require_once get_template_directory() . "/inc/template-athletes.php";
+require_once get_template_directory() . "/inc/template-functions.php";
+require_once get_template_directory() . "/inc/ballstreet-types.php";
